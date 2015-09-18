fd009f2cb733ead45fcbe62fed7773725c4efdcc
Matthew Boyette <Dyndrilliac@gmail.com>
2015-09-18 17:43:32 -0400

    Adjusted Header/Footer Code

--- functions.js
+++ functions.js
@@ -47,7 +47,9 @@ function autoSetPageSize()
 function breakFrames()
 {
 	if (self.parent.frames.length != 0)
+	{
 		self.parent.location = document.location;
+	}
 }
 
 function displaySubPage(strPageName)
@@ -55,16 +57,29 @@ function displaySubPage(strPageName)
 	$(".page_content").load(strPageName);
 }
 
-function displayClock()
+function displayFooter()
 {
 	var footers = $(".page_footer");
-	for (var i = 0; i < footers.length; i++) { footers[i].innerHTML = "<h1>" + showDateTime() + "</h1>"; }
+	var clockHTML = "<h1>" + showDateTime() + "</h1>";
+	var xhtmlHTML = "<a href=\"http://validator.w3.org/check?uri=referer\"><img src=\"http://www.w3.org/Icons/valid-xhtml11.png\" alt=\"Valid XHTML 1.1!\" /></a>";
+	var cssHTML = "<a href=\"https://jigsaw.w3.org/css-validator/check?uri=referer\"><img src=\"http://www.w3.org/Icons/valid-css2.png\" alt=\"Valid CSS 2!\" /></a>";
+	var validatorHTML = xhtmlHTML + cssHTML;
+	
+	for (var i = 0; i < footers.length; i++)
+	{
+		footers[i].innerHTML = "<p class=\"centered\">" + clockHTML + validatorHTML + "</p>";
+	}
 }
 
-function displayLogo()
+function displayHeader()
 {
 	var headers = $(".page_header");
-	for (var i = 0; i < headers.length; i++) { headers[i].innerHTML = "<img class=\"logo\" src=\"/img/logo.png\" alt=\"Logo Banner\" />"; }
+	var logoHTML = "<img class=\"logo\" src=\"http://www.unf.edu/~n00868808/img/logo.png\" alt=\"Website Logo Banner\" />";
+	
+	for (var i = 0; i < headers.length; i++)
+	{
+		headers[i].innerHTML = logoHTML;
+	}
 }
 
 function externalizeLinks()
@@ -74,14 +89,16 @@ function externalizeLinks()
 	for (var i = 0; i < links.length; i++)
 	{
 		if (links[i].getAttribute("href") && (links[i].getAttribute("rel") == "external"))
+		{
 			links[i].target = "_blank";
+		}
 	}
 }
 
 function initialize()
 {
 	breakFrames();
-	displayLogo();
+	displayHeader();
 	myTimer = setInterval("timerThread()", 333);
 }
 
@@ -109,6 +126,6 @@ function showDateTime()
 function timerThread()
 {
 	autoSetPageSize();
-	displayClock();
+	displayFooter();
 	externalizeLinks();
 }
