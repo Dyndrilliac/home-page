edb3ec75750b978bbb08d0e0152667d654273f3d
Matthew Boyette <Dyndrilliac@gmail.com>
2016-11-03 03:37:36 -0400

    Update functions.js

--- functions.js
+++ functions.js
@@ -41,7 +41,7 @@ function autoSetPageSize()
     }
 
     $("body").width((intWidth - 30) + "px");
-    $("body").height((intHeight - 30) + "px");
+    $("body").height((intHeight - 35) + "px");
 }
 
 function breakFrames()
